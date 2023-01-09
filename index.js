877566cb58e83e2a3ba5d8c89cb99eb4c0020c3b
stoian <sto14n@yahoo.com>
2023-01-09 23:36:03 +0200

    add user schema and connect db

--- index.js
+++ index.js
@@ -1,18 +1,52 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
+import { validationResult } from "express-validator";
+
+import { registerValidation } from "./validations/auth.js";
+import UserModel from "./models/User.js";
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:Qwerty123@cluster0.uustauv.mongodb.net/blog?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("DB Conected");
+  })
+  .catch((err) => console.log("DB ERROR"));
 
 const app = express();
 
 app.use(express.json()); // Read Json from POST REQUEST
 
-app.get("/", (request, response) => {
-  response.send("Hello World");
-});
+app.post("/auth/register", registerValidation, async (request, response) => {
+  try {
+    const errors = validationResult(request);
+    if (!errors.isEmpty()) {
+      return response.status(400).json(errors.array());
+    }
 
-app.post("/auth/login", (request, response) => {
-  console.log(request.body);
-  response.json({
-    sucess: true,
-  });
+    const password = request.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email: request.body.email,
+      fullName: request.body.fullName,
+      avatarUrl: request.body.avatarUrl,
+      passwordHash,
+    });
+
+    const user = await doc.save();
+
+    response.json(user);
+  } catch (err) {
+    console.log(err);
+    response.status(500).json({
+      message: "Cant register",
+    });
+  }
 });
 
 app.listen("4444", (err) => {
